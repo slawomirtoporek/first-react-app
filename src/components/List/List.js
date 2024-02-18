1bbdaf340be31db568df746dedaa6667c2381d76
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-02-18 22:01:14 +0100

    add the ColumnForm component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,18 +1,33 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
+import ColumnForm from '../ColumnForm/ColumnForm';
+import { useState } from 'react';
+import shortid from 'shortid';
 
 const List = () => {
+
+  const [columns,  setColumns] = useState([
+  	{ id: 1, title: 'Books', icon: 'book' },
+    { id: 2, title: 'Movies', icon: 'film' },
+    { id: 3, title: 'Games', icon: 'gamepad' }
+  ]);
+
+ 
+
+  const addColumn = newColumn => {
+    setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon }]);
+  };
+
   return (
-    <div>
+    <div className={styles.list}>
       <header className={styles.header}>
       <h2 className={styles.title}>Things to do<span className={styles.span}>soon</span></h2>
       </header>
       <p className={styles.description}>Interesting things I want to check out</p>
       <section className={styles.columns}>
-        <Column title="Books" icon="book" />
-        <Column title="Movies" icon="gamepad" />
-        <Column title="Games" icon="film" />
+	      {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
       </section>
+      <ColumnForm action={addColumn} />
     </div>
   );
 };
--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,7 +1,7 @@
 import styles from './TextInput.module.scss';
 
-const TextInput = props => {
-    return (<input className={styles.input} type="text" placeholder={props.placeholder} />);
+const TextInput = (props) => {
+    return <input className={styles.input} value={props.value} onChange={props.onChange} placeholder={props.placeholder} type="text" />
 };
 
 export default TextInput;
