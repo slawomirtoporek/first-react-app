6214aa6c3cb74e06aff91ee18ada5999c058edf7
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-03-02 21:48:06 +0100

    add data to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,9 @@
 import { createStore } from 'redux';
+import initialState from './initialState';
+
+
 const reducer = (state, action) => {
   return state;
-};
-
-const initialState = {
-  columns: []
 };
 
 const store = createStore(
