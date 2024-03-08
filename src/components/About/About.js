e68fde220be36faf5fb2b49d234de5343585dbfc
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-03-08 11:48:20 +0100

    add the Favorite and About component

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import styles from './About.module.scss';
+
+const About = () => {
+
+  return(
+    <section className={styles.about}>
+      <h2>About</h2>
+      <p>Lorem Ipsum.</p>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,13 @@
+import styles from './Favorite.module.scss';
+
+const Favorite = () => {
+
+  return(
+    <section className={styles.favorite}>
+      <h2>Favorite</h2>
+      <p>Lorem Ipsum.</p>
+    </section>
+  );
+};
+
+export default Favorite;
