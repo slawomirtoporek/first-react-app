ced24b94edcc4b1422ff4bb05dd1e0a8c7df6466
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-03-10 00:00:11 +0100

    add Route for subpages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Home from './components/Home/Home';
 import Container from './components/Container/Container';
 import NavBar from './components/NavBar/NavBar';
+import About from './components/About/About';
+import Favorite from './components/Favorite/Favorite';
+import NoFound from './components/NoFound/NoFound';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Container>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/favorite" element={<Favorite />} />
+        </Routes>
       </Container>
     </main>
   );
