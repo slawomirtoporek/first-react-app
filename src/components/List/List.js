9aa1d05bac4ced1be53b752cea62d2f878375bc0
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-03-10 22:53:59 +0100

    add useEffect to SearchForm component, moved SearchForm component to the List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,7 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
 import ColumnForm from '../ColumnForm/ColumnForm';
+import SearchForm from '../SearchForm/SearchForm';
 import { useSelector } from 'react-redux';
 import { getElementById, getListById } from '../../redux/store';
 import { useParams } from 'react-router';
@@ -17,6 +18,7 @@ const List = () => {
         <h2 className={styles.title}>{listData.title}</h2>
       </header>
       <p className={styles.description}>{listData.description}</p>
+      <SearchForm />
       <section className={styles.columns}>
         {columns.map(column =>
           <Column
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,7 +2,7 @@ import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
 import { useDispatch } from 'react-redux';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { updateSearchString } from '../../redux/store';
 
 const SearchForm = () => {
@@ -15,6 +15,9 @@ const SearchForm = () => {
     dispatch(updateSearchString( searchString ));
     setSearchString('');
   }
+  useEffect(() => {
+    dispatch(updateSearchString(''));
+}, [dispatch]);
   
   return (
       <form onSubmit={handleSubmit} className={styles.searchForm}>
